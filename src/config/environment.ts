2c4c71516ddcec615a8f1453e60c7d8fb48b34c1
Erick Saravia <ericks@labs.aeegle.com>
2021-06-19 20:36:45 -0600

    config(environment): Added to environment jwt and database config

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,10 +1,23 @@
 import { ENVIRONMENTS } from '../repositories/enums'
+import { interfaces, enums } from "../repositories";
+
+const bankDbConfig: interfaces.DatabaseConfig = {
+  host: process.env.DATABASE_BANK_HOST,
+  database: process.env.DATABASE_BANK_DATABASE,
+  port: Number(process.env.DATABASE_BANK_PORT),
+  username: process.env.DATABASE_BANK_USER,
+  password: process.env.DATABASE_BANK_PASSWORD,
+  driver: process.env.DATABASE_BANK_DRIVER as enums.DatabaseDriver,
+}
 
 export const environment = {
   node_env: (process.env.NODE_ENV as ENVIRONMENTS) || ENVIRONMENTS.DEVELOPMENT,
   ip: process.env.NODE_ENV || '0.0.0.0',
   port: Number(process.env.PORT || 3001),
-  mongo: {
-    url: process.env.MONGOOSE_CONNECTION_URL || `mongodb://localhost:27017/test`,
+  database: {
+    bank: bankDbConfig
+  },
+  jwt: {
+    secretKey: process.env.JWT_SECRET_KEY || `123`
   }
 }
