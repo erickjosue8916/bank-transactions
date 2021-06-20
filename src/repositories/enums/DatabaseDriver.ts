df7e9c28108a8d2b8215b3f3e3debb7db8982355
Erick Saravia <ericks@labs.aeegle.com>
2021-06-19 20:33:42 -0600

    feat(enums): Added enums for project

---
+++ src/repositories/enums/DatabaseDriver.ts
@@ -0,0 +1,7 @@
+export enum DatabaseDriver {
+  MY_SQL = 'mysql',
+  MARIA_DB = 'mariadb',
+  POSTGRE_SQL = 'postgres',
+  COCKROACH_DB = 'cockroachdb',
+  MONGO_DB = 'mongodb',
+}
--- src/repositories/enums/index.ts
+++ src/repositories/enums/index.ts
@@ -1,2 +1,3 @@
 export * from './environments'
 export * from './httpStatusCode'
+export * from './DatabaseDriver'
