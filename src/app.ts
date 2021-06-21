ebe4cae280001b30198cb31d35c50c1d3266a430
Erick Saravia <ericks@labs.aeegle.com>
2021-06-21 17:26:04 -0600

    test: testing microservice

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { Loader } from "./repositories/interfaces";
 
 
 (async () => {
+  console.log(process.env)
   const loader = container.get<Loader>(TYPES.MainLoader)
   await loader.initialize()
 })()
